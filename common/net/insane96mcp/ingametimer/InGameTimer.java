a77f233dca38929b5eeccb3d3a4b87220b4f6977
Alberto Del Villano <insane96mcp@gmail.com>
2018-03-21 09:54:29 +0100

    1.0.1 works in every version from 1.10 to 1.12

--- common/net/insane96mcp/ingametimer/InGameTimer.java
+++ common/net/insane96mcp/ingametimer/InGameTimer.java
@@ -16,9 +16,9 @@ public class InGameTimer {
 	
 	public static final String MOD_ID = "ingametimer";
 	public static final String MOD_NAME = "In Game Timer";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
-	public static final String MINECRAFT_VERSIONS = "[1.12,1.12.2]";
+	public static final String MINECRAFT_VERSIONS = "[1.10,1.12.2]";
 	
 	public static Logger logger;
 	
